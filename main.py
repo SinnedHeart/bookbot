1c606a476cdf1bbb660ffd5f81cf66d8bf8786e6
SinnedHeart <irrealco@gmail.com>
2025-08-15 21:37:01 -0600

    finalfinal

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import sys
-from stats import get_word_count, get_character_count, sort_character_count
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-def get_book_text(book_path):
-    with open(book_path) as f:
-        return f.read()
-#   # This function reads the content of the book from the specified path
-#   # and returns it as a string.
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
     book_path = sys.argv[1]
-    # Check if the book file exists
-    c_count = get_character_count(get_book_text(book_path))
-    # This function counts the occurrences of each character in the book text.
-    # It returns a dictionary where keys are characters and values are their counts.
-    num_words = get_word_count(get_book_text(book_path))
-    # This function counts the number of words in the book text.
-    # It returns the total word count as an integer.
-    print (
-        f"============ BOOKBOT ============\n"
-        f"Analyzing book found at {book_path}...\n"
-        f"----------- Word Count -----------\n"
-        f"Found {num_words} total words\n"
-        f"------- Character Count -------"
-    )
-    sorted_count = sort_character_count(c_count)
-    for char in sorted_count:
-        print(f"{char['char']}: {char['num']}")
-        # This prints the character and its count in descending order of occurrences.
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-main ()
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+main()
--- stats.py
+++ stats.py
@@ -1,25 +1,26 @@
-def get_word_count(text):
-    return len(text.split())
-# This function counts the number of words in the book text.
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_character_count(text):
-    char_count = {}
-    for char in text.lower():
-        if char not in char_count:
-            char_count[char] = 1
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            char_count[char] += 1
-    return char_count
-# This function counts the occurrences of each character in the book text.
-# It returns a dictionary where keys are characters and values are their counts.
+            chars[lowered] = 1
+    return chars
 
 
-def sort_character_count(character_count):
-    sorted_count = []
-    for char, num in character_count.items():
-        if char.isalpha():
-            sorted_count.append({'char': char, 'num': num})
-    sorted_count.sort(reverse=True, key=lambda x: x['num'] )
-    return sorted_count
-# This function sorts the character count dictionary by the number of occurrences
-# in descending order and returns a list of dictionaries with character and count.
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
